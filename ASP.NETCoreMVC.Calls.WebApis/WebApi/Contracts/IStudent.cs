4850416d1eff67d0ffa056a6b616dc0743234036
adeto <adelina_petrova91@abv.bg>
2021-03-27 13:49:05 +0200

    Push State: Add InitialMigrate in WebApi

---
+++ ASP.NETCoreMVC.Calls.WebApis/WebApi/Contracts/IStudent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Contracts
+{
+    public interface IStudent
+    {
+        int Id { get; }
+        string Name { get; }
+    }
+}
---
+++ ASP.NETCoreMVC.Calls.WebApis/WebApi/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Data;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class StudentController : ApiController
+    {
+        private Context context;
+        public StudentController(Context context)
+        {
+            this.context = context;
+        }
+
+        // Get All Students
+        [HttpGet]
+        public List<Student> Get()
+        {
+            return context.Students.ToList();
+        }
+    }
+}
